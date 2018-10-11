b077bd5cfb144d6cd00549272f36e5e3df54bc7a
Matija Marohnić <matija.marohnic@gmail.com>
2018-10-11 11:21:26 +0200

    Add bower_components to exclude patterns
    Gatsby does this as well.

--- gatsby-node.js
+++ gatsby-node.js
@@ -19,7 +19,7 @@ exports.onCreateWebpackConfig = ({ actions, stage }) => {
       rules: [
         {
           test: /\.jsx?$/,
-          exclude: /node_modules/,
+          exclude: /(node_modules|bower_components)/,
           use: [
             {
               loader: 'linaria/loader',
