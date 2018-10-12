4fa121117e58b1396cbb480faeb580b88474253d
Matija Marohnić <matija.marohnic@gmail.com>
2018-10-12 22:02:31 +0200

    Mutate JS rule instead of creating another one
    Previously we ended up with two JS rules, roughly:
    
    { test: /\.js$/, loader: 'babel-loader' }
    { test: /\.js$/, loader: 'linaria/loader' }
    
    Now we're amending the JS rule, so we get this:
    
    {
      test: /\.js$/,
      use: ['babel-loader', 'linaria/loader']
    }
    
    They are probably same or very similar, but the latter seems like better
    practice in the long run.

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,24 +12,17 @@ exports.onCreateBabelConfig = ({ actions, stage }, pluginOptions) => {
   })
 }
 
-exports.onCreateWebpackConfig = ({ actions, stage }) => {
-  const { setWebpackConfig } = actions
-  setWebpackConfig({
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /(node_modules|bower_components)/,
-          use: [
-            {
-              loader: 'linaria/loader',
-              options: {
-                sourceMap: stage.includes('develop'),
-              },
-            },
-          ],
-        },
-      ],
+exports.onCreateWebpackConfig = ({ actions, getConfig, rules, stage }) => {
+  const { replaceWebpackConfig } = actions
+  const config = getConfig()
+  const jsRule = config.module.rules.find(
+    ({ test }) => test.source === rules.js().test.source
+  )
+  jsRule.use.push({
+    loader: 'linaria/loader',
+    options: {
+      sourceMap: stage.includes('develop'),
     },
   })
+  replaceWebpackConfig(config)
 }
