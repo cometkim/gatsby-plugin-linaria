940b273dd24d959a812865307189023ce19c9d23
Haz <hazdiego@gmail.com>
2018-12-31 18:14:01 -0200

    Abstract tests

--- gatsby-node.js
+++ gatsby-node.js
@@ -23,15 +23,18 @@ exports.onCreateWebpackConfig = ({ actions, getConfig, rules, stage }) => {
     },
   }
 
+  const tests = [rules.js().test.source, '\\.tsx?$']
+
   const sourceRules = config.module.rules.filter(({ test }) => {
     if (!test) return false
-    return [rules.js().test.source, '\\.tsx?$'].includes(test.source)
+    return tests.includes(test.source)
   })
 
   sourceRules.forEach(rule => {
     if (Array.isArray(rule.use)) {
       rule.use.push(linariaConfig)
     } else if (rule.use) {
+      // gatsby-plugin-typescript doesn't put it in an array
       rule.use = [rule.use, linariaConfig]
     }
   })
