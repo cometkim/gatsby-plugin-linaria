bda0d9cd9accd2d9cefd7db566e68c5a4d8650ac
Haz <hazdiego@gmail.com>
2018-12-31 18:10:07 -0200

    Support gatsby-plugin-typescript

--- gatsby-node.js
+++ gatsby-node.js
@@ -15,14 +15,26 @@ exports.onCreateBabelConfig = ({ actions, stage }, pluginOptions) => {
 exports.onCreateWebpackConfig = ({ actions, getConfig, rules, stage }) => {
   const { replaceWebpackConfig } = actions
   const config = getConfig()
-  const jsRule = config.module.rules.find(
-    ({ test }) => test.source === rules.js().test.source
-  )
-  jsRule.use.push({
+
+  const linariaConfig = {
     loader: 'linaria/loader',
     options: {
       sourceMap: stage.includes('develop'),
     },
+  }
+
+  const sourceRules = config.module.rules.filter(({ test }) => {
+    if (!test) return false
+    return [rules.js().test.source, '\\.tsx?$'].includes(test.source)
   })
+
+  sourceRules.forEach(rule => {
+    if (Array.isArray(rule.use)) {
+      rule.use.push(linariaConfig)
+    } else if (rule.use) {
+      rule.use = [rule.use, linariaConfig]
+    }
+  })
+
   replaceWebpackConfig(config)
 }
