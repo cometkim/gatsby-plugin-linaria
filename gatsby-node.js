6949cc9af4a2a21aa6a1a76dae75aff80ab68691
Matija Marohnić <matija.marohnic@gmail.com>
2019-03-18 12:50:08 +0100

    Fix composition with gatsby-plugin-typescript
    I needed to add Babel preset for TypeScript in order for Linaria's
    evaluation feature to work, it couldn't parse TypeScript syntax
    otherwise.
    
    Ref #9
    Ref #11

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,19 +1,10 @@
+const TYPESCRIPT_RULE_TEST = '\\.tsx?$'
+
 exports.onCreateWebpackConfig = ({ actions, getConfig, rules, stage }) => {
   const { replaceWebpackConfig } = actions
   const config = getConfig()
 
-  const linariaConfig = {
-    loader: 'linaria/loader',
-    options: {
-      sourceMap: stage.includes('develop'),
-      displayName: stage.includes('develop'),
-      babelOptions: {
-        presets: ['babel-preset-gatsby'],
-      },
-    },
-  }
-
-  const tests = [rules.js().test.source, '\\.tsx?$']
+  const tests = [rules.js().test.source, TYPESCRIPT_RULE_TEST]
 
   const sourceRules = config.module.rules.filter(({ test }) => {
     if (!test) return false
@@ -21,12 +12,24 @@ exports.onCreateWebpackConfig = ({ actions, getConfig, rules, stage }) => {
   })
 
   sourceRules.forEach(rule => {
-    if (Array.isArray(rule.use)) {
-      rule.use.push(linariaConfig)
-    } else if (rule.use) {
-      // gatsby-plugin-typescript doesn't put it in an array
-      rule.use = [rule.use, linariaConfig]
+    // gatsby-plugin-typescript doesn't put it in an array
+    if (!Array.isArray(rule.use)) {
+      rule.use = [rule.use]
     }
+
+    rule.use.push({
+      loader: 'linaria/loader',
+      options: {
+        sourceMap: stage.includes('develop'),
+        displayName: stage.includes('develop'),
+        babelOptions: {
+          presets:
+            rule.test.source === TYPESCRIPT_RULE_TEST
+              ? ['babel-preset-gatsby', '@babel/preset-typescript']
+              : ['babel-preset-gatsby'],
+        },
+      },
+    })
   })
 
   replaceWebpackConfig(config)
