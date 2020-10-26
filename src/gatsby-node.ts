d3c12150acf787438c730b2b241b179867ce53a5
Hyeseong Kim <cometkim.kr@gmail.com>
2020-10-27 03:11:57 +0900

    chore: cleanup codes

--- src/gatsby-node.ts
+++ src/gatsby-node.ts
@@ -1,20 +1,23 @@
 import type * as Webpack from 'webpack';
 import type { GatsbyNode } from 'gatsby';
-import type { DeepRequired } from 'utility-types';
-import type { Maybe } from './utils';
 
-import { probably, TS_RULE_TEST } from './utils';
+import { TS_RULE_TEST } from './utils';
+
+type Falsy = false | null | undefined | 0 | '';
+type Conditional<T> = T | Falsy;
+function isTruthy<T>(condition: Conditional<T>): condition is Exclude<T, Falsy> {
+  return Boolean(condition);
+}
 
 export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = ({
-  actions,
+  actions: { setWebpackConfig, replaceWebpackConfig },
   getConfig,
   rules,
   stage,
 }) => {
-  const { setWebpackConfig, replaceWebpackConfig } = actions;
-  const config = getConfig() as DeepRequired<Webpack.Configuration>;
+  const config = getConfig() as Webpack.Configuration;
   const isDevelop = stage.startsWith('develop');
-  const usingTS = config.module.rules.some(
+  const usingTS = config.module?.rules?.some(
     ({ test }) => (test instanceof RegExp) && test.source === TS_RULE_TEST,
   );
   const linariaLoader: Webpack.NewLoader = {
@@ -26,7 +29,7 @@ export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = ({
         presets: [
           'babel-preset-gatsby',
           usingTS && '@babel/preset-typescript',
-        ].filter(probably),
+        ].filter(isTruthy),
       },
     },
   };
@@ -34,25 +37,25 @@ export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = ({
     ...rules.js(),
     use: [linariaLoader],
   };
-  const tsRule: Maybe<Webpack.RuleSetRule> = usingTS && {
+  const tsRule: Conditional<Webpack.RuleSetRule> = usingTS && {
     test: new RegExp(TS_RULE_TEST),
     use: [linariaLoader],
   };
   setWebpackConfig({
     module: {
-      rules: [jsRule, tsRule].filter(probably),
+      rules: [jsRule, tsRule].filter(isTruthy),
     },
   });
 
   // Linaria evaluator cannot handle esmodule syntax
   // @See https://github.com/cometkim/gatsby-plugin-linaria/issues/19
-  const newConfig = getConfig() as DeepRequired<Webpack.Configuration>;
-  delete newConfig.resolve.alias['@reach/router'];
+  const newConfig = getConfig() as Webpack.Configuration;
+  delete newConfig.resolve?.alias?.['@reach/router'];
   replaceWebpackConfig(newConfig);
 
   if (config.optimization) {
     // Split chunk for linaria stylesheets
-    const newConfig = getConfig() as DeepRequired<Webpack.Configuration>;
+    const newConfig = getConfig() as Webpack.Configuration;
 
     // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
     // @ts-ignore Don't try to assert this or you would do blame TypeScript
@@ -64,6 +67,7 @@ export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = ({
       // Set priority grater than default group
       priority: 1,
     };
+
     replaceWebpackConfig(newConfig);
   }
 };
